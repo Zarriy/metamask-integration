45956abd64705d012b82708db16fc6cd6c092a27
zarriy <zarriy@outlook.com>
2023-01-16 14:02:09 +0500

    developing the initial structure

--- src/etherAccount.js
+++ src/etherAccount.js
@@ -1,1 +1,27 @@
-export default function EtherAccountInfo() {}
+import { useState } from "react";
+
+export default function EtherAccountInfo() {
+  const [accountAddress, setAccountAddress] = useState(null);
+  const [accountBalance, setAccountBalance] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const connectToWalletHandler = () => {
+    if (window.ethereum) {
+      console.log("Condition true");
+    } else {
+      setErrorMessage("Please Install Metamask");
+    }
+  };
+
+  return (
+    <div className="text-center">
+      <h2>Etherium Account Detail</h2>
+      <button onClick={connectToWalletHandler}>Connect Wallet</button>
+      <div>
+        <h3>Account: {accountAddress}</h3>
+        <h3>Balance: {accountBalance}</h3>
+      </div>
+      {errorMessage}
+    </div>
+  );
+}
